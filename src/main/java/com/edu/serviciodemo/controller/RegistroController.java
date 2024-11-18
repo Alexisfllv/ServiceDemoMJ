3ee794dfafbaaefed4e29e39b164359d65f8514d
Alexisfllv <alexis-fl-live@hotmail.com>
2024-11-18 13:05:05 -0500

    crud service

--- src/main/java/com/edu/serviciodemo/controller/RegistroController.java
+++ src/main/java/com/edu/serviciodemo/controller/RegistroController.java
@@ -6,9 +6,7 @@ import com.edu.serviciodemo.service.impl.RegistroServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,9 +19,34 @@ public class RegistroController {
     private final RegistroServiceImpl service;
 
     @GetMapping("/listar")
-    public ResponseEntity<GenericResponse<Registro>> listar() throws Exception {
+    public ResponseEntity<List<Registro>> listar() throws Exception {
         List<Registro>  listado = service.listarTodo();
-        return new ResponseEntity<>(new GenericResponse<>(listado), HttpStatus.OK);
+        return new ResponseEntity<>(listado, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Registro> buscar(@PathVariable ("id") Integer id) throws Exception {
+        Registro buscarPorId = service.buscarPorId(id);
+        return new ResponseEntity<>(buscarPorId,HttpStatus.OK);
+    }
+
+    @PostMapping("/registrar")
+    public ResponseEntity<Registro> registrar(@RequestBody Registro registro) throws Exception {
+        Registro add =  service.salvar(registro);
+        return new ResponseEntity<>(add, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Registro> update(@PathVariable("id") Integer id, @RequestBody Registro registro) throws Exception {
+        registro.setIdRegistro(id);
+        Registro upd =  service.alterar(id,registro);
+        return new ResponseEntity<>(upd, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Integer id) throws Exception {
+        service.excluir(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 
